ccf51a574d441647b29afbfd926e4996f45a760c
Andriu <131737348+Andriu28@users.noreply.github.com>
2025-11-26 18:54:05 -0400

    ajuste en la forma que crea el usuario por medio de la linea de comando

--- app/Console/Commands/CreateAdminUser.php
+++ app/Console/Commands/CreateAdminUser.php
@@ -10,42 +10,20 @@ use Illuminate\Support\Facades\Validator;
 class CreateAdminUser extends Command
 {
     protected $signature = 'create:admin';
-    protected $description = 'Create the first admin user for the application';
+    protected $description = 'crea el primer usuario administrador para el sistema';
 
     public function handle()
     {
-        $this->info('Creando el primer usuario administrador...');
-
-        $name = $this->ask('Nombre del administrador');
-        $email = $this->ask('Correo electrónico del administrador');
-        $password = $this->secret('Contraseña del administrador');
-
-        $validator = Validator::make([
-            'name' => $name,
-            'email' => $email,
-            'password' => $password,
-        ], [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8'],
-        ]);
-
-        if ($validator->fails()) {
-            $this->error('Error de validación. Por favor, revisa la información proporcionada.');
-            foreach ($validator->errors()->all() as $error) {
-                $this->error($error);
-            }
-            return Command::FAILURE;
-        }
 
         User::create([
-            'name' => $name,
-            'email' => $email,
-            'password' => Hash::make($password),
-            'role' => 'administrador', // <-- Aquí asignamos el rol
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => now(), 
+            'password' => Hash::make('12345678'),
+            'role' => 'administrador',
         ]);
 
-        $this->info("¡Usuario administrador '$email' creado con éxito!");
+        $this->info("¡Usuario administrador creado con éxito!");
         return Command::SUCCESS;
     }
 }
