c2878af723cf1934cf228b5c852dacabdf983a18
TheHttMaster <diperishilla2468@gmail.com>
2026-01-11 14:33:41 -0400

    segundo commit con el intento del icono para produccion

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,8 +2,12 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-   
-    <link rel="shortcut icon" href="/favicon.ico" />
+
+    <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+    <link rel="shortcut icon" href="/favicon/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <link rel="manifest" href="/site.webmanifest" />
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
