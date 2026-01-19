d7778dbfb1755ba0744aea1314ec7e79b3bc0d29
TheHttMaster <diperishilla2468@gmail.com>
2026-01-19 08:34:57 -0400

    commit con el manual de ususario

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -276,8 +276,12 @@
                            
                             <a href="{{ route('profile.edit') }}" class="block px-4 md:px-6 py-2 md:py-3 text-sm text-gray-700 dark:text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-700/50 rounded-lg mx-2 transition-all duration-300 hover:scale-[1.03] hover:-translate-y-0.5 mb-1">Perfil</a>
                                                     
-                            <a href="#" class="block px-4 md:px-6 py-2 md:py-3 text-sm text-gray-700 dark:text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-700/50 rounded-lg mx-2 transition-all duration-300 hover:scale-[1.03] hover:-translate-y-0.5 mb-1">Configuración</a>
-                            <a href="#" class="block px-4 md:px-6 py-2 md:py-3 text-sm text-gray-700 dark:text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-700/50 rounded-lg mx-2 transition-all duration-300 hover:scale-[1.03] hover:-translate-y-0.5 mb-1">Soporte</a>
+                            
+                            <a href="{{ asset('docs/manual.pdf') }}" 
+                                target="_blank"
+                                class="block px-4 md:px-6 py-2 md:py-3 text-sm text-gray-700 dark:text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-700/50 rounded-lg mx-2 transition-all duration-300 hover:scale-[1.03] hover:-translate-y-0.5 mb-1">
+                                Soporte
+                            </a>
                             <div class="border-t border-gray-400/70 dark:border-gray-700 my-2"></div>
                             <form method="POST" action="{{ route('logout') }}" class="mx-2">
                                 @csrf
