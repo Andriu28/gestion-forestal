ab782c1c7ec7515ecd429eec4a74e24959702fb1
TheHttMaster <diperishilla2468@gmail.com>
2026-01-20 21:21:57 -0400

    segunda mejora del editar

--- routes/web.php
+++ routes/web.php
@@ -157,6 +157,10 @@ Route::middleware(['auth', 'verified'])->group(function () {
     // LUEGO la ruta resource (genérica)
     Route::resource('polygons', PolygonController::class);
 
+    Route::resource('polygons', PolygonController::class);
+    Route::get('polygons/{polygon}/edit', [PolygonController::class, 'edit'])->name('polygons.edit');
+    Route::put('polygons/{polygon}', [PolygonController::class, 'update'])->name('polygons.update');
+
     Route::get('/polygons/{polygon}/details', [PolygonController::class, 'details'])
     ->name('polygons.details')
     ->middleware('auth');
