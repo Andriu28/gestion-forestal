05bc73654b7919140e75679df88a7a24a8fd1af7
TheHttMaster <diperishilla2468@gmail.com>
2026-06-26 18:51:33 -0400

    commit simpre con detalle estetico

--- resources/views/admin/audit_log.blade.php
+++ resources/views/admin/audit_log.blade.php
@@ -204,7 +204,7 @@
 
     {{-- 4. Sin detalles --}}
     @else
-        <span class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800 dark:bg-gray-700 dark:text-gray-300">
+        <span class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-200 text-gray-800 dark:bg-gray-700 dark:text-gray-300">
             Sin detalles
         </span>
     @endif
