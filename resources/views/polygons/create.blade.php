b8b0ece30b13ac35a1796f9ff45b2e64dcbd369b
TheHttMaster <diperishilla2468@gmail.com>
2026-09-08 10:19:57 -0400

    cambio al mapa de vegetacion

--- resources/views/polygons/create.blade.php
+++ resources/views/polygons/create.blade.php
@@ -485,7 +485,7 @@ class PolygonMap {
         this.baseLayers = {
             osm: new ol.layer.Tile({
                 title: 'OpenStreetMap',
-                visible: true,
+                visible: false, // CAMBIADO: false
                 source: new ol.source.XYZ({
                     url: 'https://{a-c}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
                     attributions: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
@@ -496,7 +496,7 @@ class PolygonMap {
                     url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
                     attributions: 'Tiles © Esri'
                 }),
-                visible: false,
+                visible: false, // CAMBIADO: false
                 title: 'Satélite Esri'
             }),
             terrain: new ol.layer.Tile({
@@ -504,7 +504,7 @@ class PolygonMap {
                     url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Shaded_Relief/MapServer/tile/{z}/{y}/{x}',
                     attributions: 'Tiles © Esri'
                 }),
-                visible: false,
+                visible: false, // CAMBIADO: false
                 title: 'Relieve'
             }),
             dark: new ol.layer.Tile({
@@ -512,7 +512,7 @@ class PolygonMap {
                     url: 'https://{a-c}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png',
                     attributions: '© CartoDB'
                 }),
-                visible: false,
+                visible: false, // CAMBIADO: false
                 title: 'Oscuro'
             }),
             maptiler_satellite: new ol.layer.Tile({
@@ -522,7 +522,7 @@ class PolygonMap {
                     tileSize: 512,
                     maxZoom: 20
                 }),
-                visible: false,
+                visible: true, // CAMBIADO: true - Esta será la capa principal
                 title: 'MapTiler Satélite'
             })
         };
@@ -562,7 +562,19 @@ class PolygonMap {
             })
         });
 
-        this.currentBaseLayer = this.baseLayers.osm;
+        this.currentBaseLayer = this.baseLayers.maptiler_satellite; // CAMBIADO
+
+        // Actualizar el texto del botón (NUEVO)
+        const buttonElement = document.getElementById('base-map-toggle');
+        if (buttonElement) {
+            buttonElement.innerHTML = `
+                <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 20l-5.447-2.724A1 1 0 013 16.382V5.618a1 1 0 011.447-.894L9 7m0 13l6-3m-6 3V7m6 10l4.553 2.276A1 1 0 0021 18.382V7.618a1 1 0 00-.553-.894L15 4m0 13V4m0 0L9 7"></path>
+                </svg>
+                MapTiler Satélite
+            `;
+        }
+
         console.log('Instancia del mapa creada:', this.map);
     }
 
